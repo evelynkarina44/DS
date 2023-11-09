ad93ef2e2afcd92a1642f4944ba18c6699027900
evelynkarina44 <evelyn.goncalves12@etec.sp.gov.br>
2023-11-09 12:10:03 -0300

    Commit asp

---
+++ AplicacaoWebDS/Controllers/ClienteController.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AplicacaoWebDS.Models;
+
+namespace AplicacaoWebDS.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            var cliente = new Cliente();
+            return View(cliente);
+        }
+        [HttpPost]
+        public ActionResult Index (Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Resultado", cliente);
+            }
+            return View(cliente);
+        }
+        public ActionResult Resultado (Cliente cliente)
+        {
+            return View(cliente);
+        }
+
+    }
+}
---
+++ AplicacaoWebDS/Models/Cliente.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+
+
+namespace AplicacaoWebDS.Models
+{
+    public class Cliente
+    {
+        private int CodCliente { get; set; }
+
+        [Display(Name = "Nome do Cliente")]
+        [Required(ErrorMessage = "Por favor preencha o campo Nome do Cliente")]
+        [StringLength(50, MinimumLength = 3)]
+        public string Nome { get; set; }
+
+
+        [Display(Name = "Endereço")]
+        [Required(ErrorMessage = "Por favor preencha o campo Endereço")]
+        // [StringLength(50, MinimumLength = 4)]
+        public string Endereco { get; set; }
+
+
+        [Display(Name = "Telefone")]
+        [Required(ErrorMessage = "Por favor preencha o campo Telefone")]
+        [StringLength(12)]
+        public string Telefone { get; set; }
+
+
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Por favor preencha o campo Email")]
+        [RegularExpression(@"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*")]
+        public string Email { get; set; }
+
+
+        [Display(Name = "CPF")]
+        [Required(ErrorMessage = "Por favor preencha o campo CPF")]
+        [StringLength(11)]
+        public string CPF { get; set; }
+
+
+        [Display(Name = "Data Nascimento")]
+        [Required(ErrorMessage = "Por favor preencha o campo Data Nascimento")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime DataNasc { get; set; }
+
+    }
+}
